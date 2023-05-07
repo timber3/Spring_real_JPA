934d877c1574642fc3143d5c7605144f726c81b9
timber3 <timber3@naver.com>
2023-05-07 12:54:43 +0900

    05.07 JPA Start

---
+++ src/test/java/com/real_JPA/JPA_SHOP/JpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.real_JPA.JPA_SHOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

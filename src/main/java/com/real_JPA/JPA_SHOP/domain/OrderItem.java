e70ab40a30e575098e63a4cfffd30544686d2de1
timber3 <timber3@naver.com>
2023-05-21 23:59:30 +0900

    05.21 Order,OrderItem 생성, 비즈니스 메서드

--- src/main/java/com/real_JPA/JPA_SHOP/domain/OrderItem.java
+++ src/main/java/com/real_JPA/JPA_SHOP/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package com.real_JPA.JPA_SHOP.domain;
 
 import com.real_JPA.JPA_SHOP.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,6 +11,7 @@ import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -26,6 +29,11 @@ public class OrderItem {
     private int orderPrice; // 주문당시 가격
 
     private int count; // 주문 수량
+
+    // 쓰지 못하게 protected로 설정해버림. -> 이거 대신에 위에 @NoArgsConstructor로 대체함.
+//    protected OrderItem() {
+//
+//    }
 
     // == 생성 메서드 == //
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
--- src/main/java/com/real_JPA/JPA_SHOP/service/OrderService.java
+++ src/main/java/com/real_JPA/JPA_SHOP/service/OrderService.java
@@ -35,6 +35,10 @@ public class OrderService {
         delivery.setAddress(member.getAddress());
 
         // 주문상품 생성
+        // OrderItem은 static 메서드로 생성하게 되는데
+        // 누군가는 new 생성으로 대입하는 사람이 있을것이다.
+        // 어디서는 static메서드로, 어디서는 new로 생성하게 된다면 유지보수의 관점에서 좋지않다.
+        // 그래서 기본 생성자를 protected로 막아준다.
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
 
         // 주문 생성
@@ -44,5 +48,13 @@ public class OrderService {
         orderRepository.save(order);
 
         return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+
     }
 }

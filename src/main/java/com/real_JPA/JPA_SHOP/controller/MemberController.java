e7ee108fb1ffc1ece956541b025bd2405cec0e2a
timber3 <timber3@naver.com>
2023-06-16 23:44:51 +0900

    06.16

---
+++ src/main/java/com/real_JPA/JPA_SHOP/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.real_JPA.JPA_SHOP.controller;
+
+import com.real_JPA.JPA_SHOP.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+
+    }
+
+}
